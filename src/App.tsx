eb9e29ae29068517d29e9116f96cc6c9d421438f
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-12-22 14:59:37 +0100

    convert from class to function

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
 import _ from "lodash"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { addDays, endOfDay, formatISO } from "date-fns"
 import "./App.css"
 import TrainAnnouncement from "./TrainAnnouncement"
 
 let eventSource: EventSource | null = null
 
-type AppState = {
-  announcements: TrainAnnouncement[]
-  locations: { [key: string]: string }
-  clicked: string
-}
+export default function App() {
+  const [announcements, setAnnouncements] = useState<TrainAnnouncement[]>([])
+  const [locations, setLocations] = useState<{ [key: string]: string }>({})
+  const [clicked, setClicked] = useState("")
 
-export default class App extends React.Component<{}, AppState> {
-  state: AppState = { announcements: [], locations: {}, clicked: "" }
-
-  async componentDidMount() {
-    const response = await fetch("/.netlify/functions/locations")
-    const locations = await response.json()
-    this.setState({ locations })
-  }
-
-  componentWillUnmount() {
-    if (eventSource) {
-      eventSource.close()
-      eventSource = null
+  useEffect(() => {
+    async function fetchLocations() {
+      const response = await fetch("/.netlify/functions/locations")
+      setLocations(await response.json())
     }
-  }
 
-  getCurrent(trainId: string) {
+    function cleanup() {
+      if (eventSource) {
+        eventSource.close()
+        eventSource = null
+      }
+    }
+
+    fetchLocations()
+
+    return cleanup
+  }, [])
+
+  function getCurrent(trainId: string) {
     return async (ev: any) => {
       if (ev.key && ev.key !== "Enter") return
 
@@ -45,96 +46,88 @@ export default class App extends React.Component<{}, AppState> {
         eventSource = new EventSource(json.INFO.SSEURL)
         eventSource.onmessage = (event) => {
           const parsed = JSON.parse(event.data)
-          this.setState(({ announcements }) => ({
-            announcements: announcements.concat(
-              parsed.RESPONSE.RESULT[0].TrainAnnouncement
-            ),
-          }))
+          setAnnouncements(
+            announcements.concat(parsed.RESPONSE.RESULT[0].TrainAnnouncement)
+          )
         }
       }
 
-      this.setState({
-        announcements,
-        clicked: "",
-      })
+      setAnnouncements(announcements)
+      setClicked("")
     }
   }
 
-  render() {
-    const sorted: TrainAnnouncement[] = _.orderBy(
-      this.state.announcements,
-      "AdvertisedTimeAtLocation"
-    )
+  const sorted: TrainAnnouncement[] = _.orderBy(
+    announcements,
+    "AdvertisedTimeAtLocation"
+  )
 
-    const rowKeys: string[] = _.uniq(_.map(sorted, activityAndLocation))
-    const colKeys: string[] = _.uniq(_.map(sorted, date))
-    const cells: {
-      [key: string]: TrainAnnouncement
-    } = _.keyBy(
-      sorted,
-      (a: TrainAnnouncement): string => activityAndLocation(a) + date(a)
-    )
+  const rowKeys: string[] = _.uniq(_.map(sorted, activityAndLocation))
+  const colKeys: string[] = _.uniq(_.map(sorted, date))
+  const cells: {
+    [key: string]: TrainAnnouncement
+  } = _.keyBy(
+    sorted,
+    (a: TrainAnnouncement): string => activityAndLocation(a) + date(a)
+  )
 
-    return (
-      <div>
-        <input
-          type="text"
-          value={this.state.clicked}
-          onChange={(event: any) => {
-            this.setState({ clicked: event.target.value })
-          }}
-          onKeyPress={this.getCurrent(this.state.clicked)}
-        />
-        <span onClick={this.getCurrent(this.state.clicked)}>submit</span>
-        <table>
-          <tbody>
-            <tr>
-              <th>location</th>
-              {_.map(colKeys, (colKey) => {
-                return (
-                  <React.Fragment key={colKey}>
-                    <th />
-                    <th>{colKey.slice(8)}</th>
-                  </React.Fragment>
-                )
-              })}
-            </tr>
-            {_.map(rowKeys, (rowKey) => {
-              const strings = rowKey.split(":")
+  return (
+    <div>
+      <input
+        type="text"
+        value={clicked}
+        onChange={(event: any) => {
+          setClicked(event.target.value)
+        }}
+        onKeyPress={getCurrent(clicked)}
+      />
+      <span onClick={getCurrent(clicked)}>submit</span>
+      <table>
+        <tbody>
+          <tr>
+            <th>location</th>
+            {_.map(colKeys, (colKey) => {
               return (
-                <tr key={rowKey}>
-                  <td>
-                    {strings[0] === "Avgang" ? "" : strings[0]}{" "}
-                    {this.state.locations[strings[1]]
-                      ? this.state.locations[strings[1]]
-                      : strings[1]}
-                  </td>
-                  {_.map(colKeys, (colKey) => {
-                    const cell = cells[rowKey + colKey]
-                    if (!cell)
-                      return (
-                        <React.Fragment key={colKey}>
-                          <td />
-                          <td />
-                        </React.Fragment>
-                      )
-                    const t = cell.TimeAtLocationWithSeconds
-                    const a = cell.AdvertisedTimeAtLocation
-                    return (
-                      <React.Fragment key={colKey}>
-                        <td>{t ? t.slice(11, 19) : "-"}</td>
-                        <td>{a ? a.slice(11, 16) : "-"}</td>
-                      </React.Fragment>
-                    )
-                  })}
-                </tr>
+                <React.Fragment key={colKey}>
+                  <th />
+                  <th>{colKey.slice(8)}</th>
+                </React.Fragment>
               )
             })}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+          </tr>
+          {_.map(rowKeys, (rowKey) => {
+            const strings = rowKey.split(":")
+            return (
+              <tr key={rowKey}>
+                <td>
+                  {strings[0] === "Avgang" ? "" : strings[0]}{" "}
+                  {locations[strings[1]] ? locations[strings[1]] : strings[1]}
+                </td>
+                {_.map(colKeys, (colKey) => {
+                  const cell = cells[rowKey + colKey]
+                  if (!cell)
+                    return (
+                      <React.Fragment key={colKey}>
+                        <td />
+                        <td />
+                      </React.Fragment>
+                    )
+                  const t = cell.TimeAtLocationWithSeconds
+                  const a = cell.AdvertisedTimeAtLocation
+                  return (
+                    <React.Fragment key={colKey}>
+                      <td>{t ? t.slice(11, 19) : "-"}</td>
+                      <td>{a ? a.slice(11, 16) : "-"}</td>
+                    </React.Fragment>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 function activityAndLocation(a: TrainAnnouncement): string {
